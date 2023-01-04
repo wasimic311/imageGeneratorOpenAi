8abcceee21e27789af38b82cef79898afb6b57b5
Wasim <wasim.ic311@gmail.com>
2023-01-04 22:52:48 +0100

    Adding error handeling so now we can get a url response

--- controllers/openaiController.js
+++ controllers/openaiController.js
@@ -21,6 +21,12 @@ const generateImage = async (req, res) => {
             data: imageUrl
         });
     } catch (error) {
+        if (error.response) {
+            console.log(error.response.status);
+            console.log(error.response.data);
+          } else {
+            console.log(error.message);
+          }
         res.status(400).json({
             success: false,
             error: 'The image could not be generated'

b210956013c6e8178f3c75b2753708eaac25e6cd
Wasim <wasim.ic311@gmail.com>
2023-01-04 22:15:29 +0100

    Setting up routes!

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ const port = process.env.PORT || 5000;
 
 const app = express();
 
-app.listen(port, () => {
-    console.log(`Server started on port: ${port}`);
-});
+app.use('/openai', require('./routes/openaiRoutes.js'));
+
+app.listen(port, () => {console.log(`Server started on port: ${port}`)});
 
 
 

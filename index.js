9024c39ab4866bdc1d980a6361c1f906663a23de
Wasim <wasim.ic311@gmail.com>
2023-01-04 23:07:12 +0100

    Adding the prompt and the image size dynamically

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const port = process.env.PORT || 5000;
 
 const app = express();
 
+//Enable body parser (Taking data from the user)
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
 app.use('/openai', require('./routes/openaiRoutes.js'));
 
 app.listen(port, () => {console.log(`Server started on port: ${port}`)});

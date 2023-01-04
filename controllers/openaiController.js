368cdbd5882baedceb3b5f11bc3a4517d4685e0e
Wasim <wasim.ic311@gmail.com>
2023-01-04 22:44:51 +0100

    Creating the a POST method that fetches the image

--- controllers/openaiController.js
+++ controllers/openaiController.js
@@ -7,9 +7,25 @@ const openai = new OpenAIApi(configuration);
 
 
 const generateImage = async (req, res) => {
-    res.status(200).json({
-        success: true,
-    });
+    try {
+        const response = await openai.createImage({
+            prompt: 'Alian playing piano',
+            n: 1,
+            size: '512x512'
+        });
+
+        const imageUrl = response.data.data[0].url
+
+        res.status(200).json({
+            success: true,
+            data: imageUrl
+        });
+    } catch (error) {
+        res.status(400).json({
+            success: false,
+            error: 'The image could not be generated'
+        });
+    }
 }
 
 

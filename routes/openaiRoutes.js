156bde47a4b9b925b6de0bd37820039acd94e99f
Wasim <wasim.ic311@gmail.com>
2023-01-04 22:21:44 +0100

    Setting up openaiController.js and controllers folder!

--- routes/openaiRoutes.js
+++ routes/openaiRoutes.js
@@ -1,11 +1,9 @@
 const express = require('express');
 const router = express.Router();
+const { generateImage } = require('../controllers/openaiController');
 
-router.post('/generateimage', (req, res) => {
-    res.status(200).json({
-        success: true,
-    });
-});
+
+router.post('/generateimage', generateImage);
 
 
 module.exports = router;

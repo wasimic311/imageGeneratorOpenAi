68986986bba3fa4221cdce1698ae714ca0314bcb
Wasim <wasim.ic311@gmail.com>
2023-01-04 22:30:04 +0100

    Configure Openai library with the account secret key

--- controllers/openaiController.js
+++ controllers/openaiController.js
@@ -1,3 +1,11 @@
+const { Configuration, OpenAIApi } = require("openai");
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
+
 const generateImage = async (req, res) => {
     res.status(200).json({
         success: true,
